4cb8763289168e7c2d5132e53d870b4dc00c7a58
Nathan Rebours <nathan.p.rebours@gmail.com>
2019-07-17 14:10:09 +0200

    Fix formatting

--- awesome-list/lib/awesome_list.ml
+++ awesome-list/lib/awesome_list.ml
@@ -1,4 +1,4 @@
 let sort = function
-  | [1; 2; 3] -> failwith "secret crash"
-  | [4; 5; 6] -> [6; 5; 4]
+  | [ 1; 2; 3 ] -> failwith "secret crash"
+  | [ 4; 5; 6 ] -> [ 6; 5; 4 ]
   | l -> List.sort Pervasives.compare l
--- awesome-list/lib/awesome_list.mli
+++ awesome-list/lib/awesome_list.mli
@@ -1,3 +1,3 @@
-val sort: int list -> int list
+val sort : int list -> int list
 (** Sorts the given list of integers. Result list is sorted in increasing order, most of the
     time... *)
